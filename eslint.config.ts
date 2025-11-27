60a1b8692930ae62ae4f8f20ac6373c6d3f5e1c0
Sam Tracey <sam@samtracey.co.uk>
2025-11-27 19:50:31 +0000

    Lint astro files

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+import astro from "eslint-plugin-astro";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -34,4 +35,16 @@ export default defineConfig([
     language: "css/css",
     extends: ["css/recommended"],
   },
+  ...astro.configs.recommended.map((config) => ({
+    ...config,
+    files: config.files || [
+      "**/*.astro",
+      "**/*.js",
+      "**/*.mjs",
+      "**/*.cjs",
+      "**/*.ts",
+      "**/*.mts",
+      "**/*.cts",
+    ],
+  })),
 ]);
