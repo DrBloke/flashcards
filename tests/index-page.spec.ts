de59285bd1e16586a02fbf668e0420243c3da6fd
Sam Tracey <sam@samtracey.co.uk>
2026-01-09 14:24:03 +0000

    Extend e-2-e tests. Include Axe testing

--- tests/index-page.spec.ts
+++ tests/index-page.spec.ts
@@ -1,15 +1,20 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
-test("Can navigate to test deck", async ({ page }) => {
+test("Index page is accessible and showing the decks", async ({ page }) => {
   await page.goto("/flashcards/test");
+  await expect(page).toHaveTitle("Test Decks");
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
 
   // Navigation to test deck is present
   const link = page.getByRole("link", { name: "Test Deck 1" });
   await expect(link).toBeVisible();
-
   // Click the link
   await link.click();
 
   // Naviagation takes effect
   await expect(page).toHaveURL("/flashcards/test/01-test-deck");
+  await expect(page).toHaveTitle("Test Deck 1");
 });
